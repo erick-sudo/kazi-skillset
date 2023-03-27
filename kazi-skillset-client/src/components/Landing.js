3062a49d321f9ea4745711a44f05389d9dbfeb56
Abel Muriithi <abelmuriithi04@gmail.com>
2023-03-27 22:10:48 +0300

    done frontend

---
+++ kazi-skillset-client/src/components/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Landing = () => {
+  return (
+    <div>
+
+      <div className='text1'>
+        <p>Get Started</p>
+      </div>
+
+      <div class="input-box">
+        <i class="uil uil-search"></i>
+        <input type="text" placeholder="Search here..." />
+        <button class="button">Search</button>
+       </div>
+  
+    </div>
+   
+  );
+}
+
+export default Landing;
