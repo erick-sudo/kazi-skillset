92a2d44b28ea24d74558f26be2e5cfce46adb979
erick-sudo <erickochieng766@gmail.com>
2023-03-28 19:34:07 +0300

    Login and Signup

---
+++ kazi-skillset-client/src/components/SignUpForm.js
@@ -0,0 +1,31 @@
+function SignUpForm(){
+    return(
+        <div>
+            <div className="flex  justify-center"> 
+                 <form className="flex flex-col px-10 bg-slate-400 max-w-lg my-8">
+                     <label>First Name:</label>
+                        <input type="text" className="label2" name="firstname"/>
+
+                        <label>Last Name:</label>
+                        <input type="text" className="label2" name="lastname"/>
+
+                        <label>Email Address:</label>
+                        <input type="email" className="label2" name="email"/>
+
+
+                        <label>Password:</label>
+                        <input type="password" className="label2" name="password"/>
+
+                        <label>Confirm Password:</label>
+                        <input type="confirm password" className="label2" name="confirm password"/>
+
+
+                <button className="bg-white w-max px-2 my-2 mx-auto">SignUp</button>
+            </form>
+          </div>
+        </div>
+        
+    )
+}
+
+export default SignUpForm;
